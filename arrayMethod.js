a4c50fceb5c9eaa64ab9facc63c535588a1d82a7
shafrinahamed0007 <mrdeveloperdevv@gmail.com>
2026-01-31 17:29:31 +0600

    Declare copywithin() method

--- arrayMethod.js
+++ arrayMethod.js
@@ -110,6 +110,13 @@ const updateArr4 = arr4.concat("Peter");
 console.log(updateArr4);
 
 
+//------------------ copyWithin() ----------------------//
 
+// The copywithin() method copies array elements to another position in an array.
+
+const fruits1 = [ "Banana", "Orange", "Apple", "Mango", "Kiwi"];
+// fruits1.copyWithin(2,0);
+fruits1.copyWithin(2,0,2);
+console.log(fruits1); 
 
 
